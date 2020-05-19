ae3f6050433f48f1344b5ab14beafaa03b4be4fb
Aurelien Dersy <aurelien@dersy.com>
2020-05-19 23:58:14 +0200

    - Initial query to get the forecasted weather

--- DL/temperature_data_download.py
+++ DL/temperature_data_download.py
@@ -4,3 +4,19 @@ Here we access the relevant database and ask for the desired temperature history
 
 """
 
+import requests
+import pandas as pd
+
+from DL.api_key import api_key
+
+api_str_ex = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689' \
+             '&exclude=minutely&appid={}'.format(api_key)
+
+
+
+response = requests.get(api_str_ex)
+
+response_data = response.json()
+hourly_data = pd.DataFrame(response_data['hourly'])
+daily_data = pd.DataFrame(response_data['daily'])
+
